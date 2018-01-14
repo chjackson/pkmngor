34be76bdca8f56a6781e1f341c54d11a3b218ab1
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2018-01-14 17:34:22 +0000

    Added pokedex IDs

--- data-raw/read_json.r
+++ data-raw/read_json.r
@@ -21,11 +21,11 @@ pkmn[,listids] <- lapply(pkmn[,listids], pad)
 pkmn <- do.call("cbind", pkmn) %>% 
   as.tbl %>% 
   filter(!is.na(pokemonId)) %>%
-  mutate(stats.baseDefPlusStam = stats.baseDefense + stats.baseStamina,
+  mutate(pokemonId = as.character(pokemonId), 
+         stats.baseDefPlusStam = stats.baseDefense + stats.baseStamina,
          stats.baseSqrtDefTimesStam = sqrt(stats.baseDefense*stats.baseStamina),
          type = sub("POKEMON_TYPE_" , "", type),
          type2 = sub("POKEMON_TYPE_" , "", type2)) 
-
 
 types <- c("GRASS"="78C850", "FIRE"="F08030", "WATER"="6890F0", "BUG"="A8B820", "NORMAL"="A8A878", 
            "POISON"="A040A0", "ELECTRIC"="F8D030","GROUND"="E0C068", "FAIRY"="EE99AC", 
@@ -37,10 +37,16 @@ types <- data.frame(type = names(types),
                     color = paste0("#", types), 
                     stringsAsFactors = FALSE)
 
-pkmn <- pkmn %>% right_join(types)
-
 pkmn <- pkmn %>% 
+  inner_join(types) %>%
   mutate(type = factor(type, levels=types$type)) # types in pokedex order
+
+## add Pokedex ids, from https://github.com/veekun/pokedex/blob/master/pokedex/data/csv/pokemon.csv
+pokedex <- read.csv("data-raw/pokemon.csv", as.is=TRUE) %>% 
+  mutate(identifier = toupper(identifier)) %>% 
+  select(id, pokemonId = identifier) 
+
+pkmn <- pkmn %>% inner_join(pokedex) %>% arrange(id)
 
 save(pkmn, file="data/pkmn.rda")
 save(types, file="data/types.rda")
